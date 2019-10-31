3246ccdcb5453d9abedfac2d7a8ee039f0a55bc7
Nathan <nwaggoner16@gmail.com>
2019-10-31 18:28:24 -0400

    update

--- hardware.py
+++ hardware.py
@@ -44,5 +44,22 @@ class moisture_sensor:
 			return 0 # off
         GPIO.cleanup()
 
+def rc_time(self):
+    count = 0
+
+    #Output on the pin for
+    GPIO.setup(phr, GPIO.OUT)
+    GPIO.output(phr, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(phr, GPIO.IN)
+
+    #Count until the pin goes high
+    while (GPIO.input(phr) == GPIO.LOW):
+        count += 1
+
+    return count
+	
 def cleanup():
 	GPIO.cleanup()
--- sprinklerpi.py
+++ sprinklerpi.py
@@ -16,25 +16,13 @@ with open("pinout.ini") as f:
     sample_config = f.read()
 config = ConfigParser.RawConfigParser(allow_no_value=True)
 config.readfp(io.BytesIO(sample_config))
+GPIO.setmode(GPIO.BOARD)
 
 #Assign pin numbers to variables
 phr = int(config.get('pinout','photoresistor'))
 dht = int(config.get('pinout','dht11'))
 ms = int(config.get('pinout','moisture_sensor'))
 
-def rc_time(phr):
-    count = 0
 
-    #Output on the pin for
-    GPIO.setup(phr, GPIO.OUT)
-    GPIO.output(phr, GPIO.LOW)
-    time.sleep(0.1)
-
-    #Change the pin back to input
-    GPIO.setup(phr, GPIO.IN)
-
-    #Count until the pin goes high
-    while (GPIO.input(phr) == GPIO.LOW):
-        count += 1
-
-    return count
+hw.rc_time(phr);
+hw.cleanup();
