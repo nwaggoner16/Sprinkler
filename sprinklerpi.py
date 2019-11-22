23f49782eaef8071bc9d4dc046d8e070d6e20d19
Nathan Waggoner <nwaggoner16@gmail.com>
2019-11-22 12:58:59 -0500

    Sprinklerpi updating

--- sprinklerpi.py
+++ sprinklerpi.py
@@ -30,11 +30,11 @@ speed=int(config.get('L298n','speed'))
 run_time=int(config.get('L298n','run_time'))
 pump1 = hw.L298n(ena, in1, in2, speed, run_time)
 
+
 #Run
 moisture = hw.moisture_sensor(ms);
-temp = hw.DHT11()
-print(temp.read_dht())
-#if (moisture.moisture_check() == 0):
-#    print('Pump running')
-#pump1.run_pump()
+
+if (moisture.moisture_check() == 0):
+    print('Pump running')
+    pump1.run_pump()
     
